6fc765a67c04aeeceb1e2c39d52883edaaa06aff
Shahzod <shahzodwill@gmail.com>
2024-09-07 18:43:31 +0200

    completed login function

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,3 +1,34 @@
+import {headers} from "../../api/headers.js";
+import {API_AUTH_LOGIN} from "../constants.js"
+
 export async function login({ email, password }) {
     
+    try {
+        const response = await fetch(API_AUTH_LOGIN, {
+          method: 'POST',
+          headers: headers(),
+          body: JSON.stringify({
+            email,
+            password
+          })
+        })
+    
+        if (!response.ok) {
+          const error = await response.json();
+          throw new Error(`Login failed: ${error.message}`);
+        }
+
+        const {data} = await response.json()
+        const {accessToken: token, ...user} = data;
+        localStorage.token = token;
+        localStorage.user = JSON.stringify(user);
+        console.log('Login successful!', data);
+        
+        // redirect to home page;
+        window.location.pathname = '../index.html';
+
+      } catch(error) {
+        console.error('Error during login:', error);
+        throw error;
+      }
 }
--- src/js/router/views/login.js
+++ src/js/router/views/login.js
@@ -3,4 +3,14 @@ import { login } from "../../api/auth/login.js";
 
 const form = document.forms.login;
 
-form.addEventListener("submit", onLogin);
+form.addEventListener("submit", () => {
+    event.preventDefault();
+    onLogin();
+    const formData = new FormData(form);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    login({
+        email, 
+        password
+    });
+});
