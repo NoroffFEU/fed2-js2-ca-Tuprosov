c6d6bbd6dbc369af924b17a1d85210d53ba5c405
Shahzod <shahzodwill@gmail.com>
2024-09-08 19:29:47 +0200

    completed submit event for post creation

--- src/js/router/views/postCreate.js
+++ src/js/router/views/postCreate.js
@@ -1,8 +1,24 @@
 import { onCreatePost } from "../../ui/post/create.js";
+import {createPost} from "../../api/post/create.js";
 import { authGuard } from "../../utilities/authGuard.js";
 
 authGuard();
 
 const form = document.forms.createPost;
 
-form.addEventListener("submit", onCreatePost);
+form.addEventListener("submit", () => {
+    event.preventDefault();
+    onCreatePost();
+    const formData = new FormData(form);
+    // Collect form data
+    const title = formData.get('title');
+    const body = formData.get('body');
+    const tags = formData.get('tags').split(',').map(tag => tag.trim()).filter(tag => tag !== "");
+    const imageUrl = formData.get('imageUrl')
+    createPost({
+        title,
+        body,
+        tags,
+        media: {"url": imageUrl}
+    }, form);
+});
