60895026d52f3785e94c35249e21ec9d6ce08ce7
Shahzod <shahzodwill@gmail.com>
2024-09-08 19:28:53 +0200

    added check for token in local storage

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -7,6 +7,10 @@ export function headers() {
     headers.append("X-Noroff-API-Key", API_KEY);
   }
 
+  if (localStorage.token) {
+    headers.append("Authorization", `Bearer ${localStorage.token}`)
+  }
+
   headers.append('Content-type', 'application/json')
 
   return headers;
