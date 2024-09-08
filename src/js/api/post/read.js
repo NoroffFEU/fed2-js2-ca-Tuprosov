79d863245e04340ea6eeb53fe759226966096bff
Shahzod <shahzodwill@gmail.com>
2024-09-08 20:20:46 +0200

    completed read post function to get 12 latest posts

--- src/js/api/post/read.js
+++ src/js/api/post/read.js
@@ -1,5 +1,28 @@
+import { API_SOCIAL_POSTS } from "../constants.js";
+import { headers } from "../headers.js";
+import { displayPosts } from "./displayPosts.js";
+
 export async function readPost(id) {}
 
-export async function readPosts(limit = 12, page = 1, tag) {}
+export async function readPosts(limit = 12, page = 1, sortOrder = "desc") {
+    try {
+        const response = await fetch(API_SOCIAL_POSTS, {
+           method: "GET",
+           headers: headers(),
+        })
+
+        if (!response.ok) {
+            const error = await response.json();
+            throw new Error(`Error displaying posts: ${error.message}`);
+        }
+
+        const data = await response.json();
+        displayPosts(data);
+
+    } catch (error) {
+        console.error('Error getting post', error);
+        throw error;
+    }
+}
 
 export async function readPostsByUser(username, limit = 12, page = 1, tag) {}
