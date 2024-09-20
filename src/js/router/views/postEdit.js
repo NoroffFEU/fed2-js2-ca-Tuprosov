42a72a075641650fa6e8e07617088f7730c20190
Shahzod <shahzodwill@gmail.com>
2024-09-20 12:42:25 +0200

    populate edit form with the post details

--- src/js/router/views/postEdit.js
+++ src/js/router/views/postEdit.js
@@ -2,7 +2,12 @@ import { onUpdatePost } from "../../ui/post/update.js";
 import { authGuard } from "../../utilities/authGuard.js";
 
 authGuard();
-
 const form = document.forms.editPost;
+const post = JSON.parse(localStorage.getItem('currentPost'));
+console.log(post)
+form.title.value = post.title || " "; 
+form.body.value = post.body || " ";
+form.imageUrl.value = post.media.url || " ";
+form.tags.value = post.tags.join(', ');
 
 form.addEventListener("submit", onUpdatePost);
