77d69edc28be67d1d8a8df5188031f1dc2ec605d
Shahzod <shahzodwill@gmail.com>
2024-09-20 11:09:06 +0200

    style for list element in listing page/set event listener

--- src/js/api/post/displayPosts.js
+++ src/js/api/post/displayPosts.js
@@ -8,13 +8,56 @@ export function displayPosts(data){
     data.forEach(post => {
         const li = document.createElement('li');
         li.setAttribute('id', `${post.id}`)
-        // Assuming each post has properties like 'title', 'body', and 'author'
+        li.style.cursor = 'pointer';
+        li.style.border = '1px solid black';
+
+        if (!post.media) {
+            createMedia(post)
+        }
+    
         li.innerHTML = `
             <h3>${post.title}</h3>
+            <img src="${post.media.url}" alt="${post.media.alt}">
             <p>${post.body}</p>
         `;
 
+        // set event listener
+        li.addEventListener('click', () => {
+            window.location.href = `index.html?id=${post.id}`
+        })
         // Append the list item to the unordered list
         ul.appendChild(li);
     });
+}
+
+function createMedia(post) {
+    post.media = {};
+    post.media.url  = 'https://as2.ftcdn.net/v2/jpg/07/50/96/13/1000_F_750961330_OBCPAVhuMuaEudMMtGgVbRHPmLM3GWKD.jpg';
+    post.media.alt = 'placeholderImage';
+    return post;
+}
+
+export function displayTargetPost(post){
+    console.log(post)
+    const title = document.getElementById('title');
+    const body = document.getElementById('body')
+    title.textContent = post.title;
+
+    const main = document.createElement('main');
+    const paragraph = document.createElement('p');
+    const img = document.createElement('img');
+
+    paragraph.textContent = post.body;
+
+    if (!post.media) {
+        createMedia(post)
+    }
+    
+    img.src = post.media.url;
+    img.alt = post.media.alt;
+    
+
+    // Append the content to main body
+    main.append(img, paragraph)
+    body.appendChild(main)
 }
