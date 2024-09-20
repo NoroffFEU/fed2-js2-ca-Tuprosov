dec044917fc6b3cdf8555de846b3a92771abed75
Shahzod <shahzodwill@gmail.com>
2024-09-20 11:08:30 +0200

    complete function for fetching single post

--- src/js/api/post/read.js
+++ src/js/api/post/read.js
@@ -1,8 +1,31 @@
-import { API_SOCIAL_POSTS } from "../constants.js";
-import { headers } from "../headers.js";
-import { displayPosts } from "./displayPosts.js";
+import {API_SOCIAL_POSTS} from "../constants.js";
+import {headers} from "../headers.js";
+import {displayPosts} from "./displayPosts.js";
+import {displayTargetPost} from "./displayPosts.js";
 
-export async function readPost(id) {}
+export async function readPost(id) {
+    const url = API_SOCIAL_POSTS + '/' + id
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: headers()
+        })
+
+        if (! response.ok) {
+            const error = await response.json();
+            throw new Error(`Error displaying the post: ${
+                error.message
+            }`)
+        }
+
+        const data = await response.json();
+        displayTargetPost(data.data)
+
+    } catch (error) {
+        console.error('Error getting post', error);
+        throw error;
+    }
+}
 
 export async function readPosts(limit = 12, page = 1, sortOrder = "desc") {
     const url = new URL(API_SOCIAL_POSTS);
@@ -11,17 +34,18 @@ export async function readPosts(limit = 12, page = 1, sortOrder = "desc") {
     url.searchParams.append('sortOrder', sortOrder);
     try {
         const response = await fetch(url, {
-           method: "GET",
-           headers: headers(),
+            method: "GET",
+            headers: headers()
         })
 
-        if (!response.ok) {
+        if (! response.ok) {
             const error = await response.json();
-            throw new Error(`Error displaying posts: ${error.message}`);
+            throw new Error(`Error displaying posts: ${
+                error.message
+            }`);
         }
 
         const data = await response.json();
-        console.log(typeof data.data)
         displayPosts(data.data);
 
     } catch (error) {
