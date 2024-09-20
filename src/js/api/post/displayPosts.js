ce729a27fbbfa9e498baf783cd77c0c9657967a9
Shahzod <shahzodwill@gmail.com>
2024-09-20 12:41:53 +0200

    update displaypost function

--- src/js/api/post/displayPosts.js
+++ src/js/api/post/displayPosts.js
@@ -1,3 +1,5 @@
+import { saveInLocalStorage } from "../../utilities/savePost.js";
+
 export function displayPosts(data){
     const ul = document.getElementById('postsList');
     ul.style.listStyle = 'none';
@@ -7,26 +9,41 @@ export function displayPosts(data){
     // Loop through the data array and create list items for each post
     data.forEach(post => {
         const li = document.createElement('li');
-        li.setAttribute('id', `${post.id}`)
+        const editBtn = document.createElement('button');
+        const deleteBtn = document.createElement('button');
+        const h3 = document.createElement('h3');
+        const img = document.createElement('img');
+        const p = document.createElement('p');
+
+        li.setAttribute('id', `${post.id}`);
         li.style.cursor = 'pointer';
         li.style.border = '1px solid black';
 
+        // populate elements
+        h3.textContent = `${post.title}`;
+        p.textContent = `${post.body}`;
         if (!post.media) {
             createMedia(post)
         }
+        img.src = post.media.url;
+        img.alt = post.media.alt;
+        editBtn.textContent = 'Edit post';
+        deleteBtn.textContent = 'Delete post';
     
-        li.innerHTML = `
-            <h3>${post.title}</h3>
-            <img src="${post.media.url}" alt="${post.media.alt}">
-            <p>${post.body}</p>
-            <button id="Edit">Edit post</button>
-            <button id="Delete">Delete post</button>
-        `;
-
-        // set event listener
+        // set event listeners
         li.addEventListener('click', () => {
             window.location.href = `index.html?id=${post.id}`
         })
+
+        editBtn.addEventListener('click', () => {
+            event.stopPropagation();
+            saveInLocalStorage(post)
+            window.location.href = `edit/index.html?id=${post.id}`
+        })
+
+        // append buttons to li
+        li.append(h3, img, p, editBtn, deleteBtn);
+    
         // Append the list item to the unordered list
         ul.appendChild(li);
     });
