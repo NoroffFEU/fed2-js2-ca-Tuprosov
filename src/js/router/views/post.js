2bf384c0d4c730693b367a806cbd03edd7312a07
Shahzod <shahzodwill@gmail.com>
2024-09-20 11:08:01 +0200

    import read fucntion and get id

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,1 +1,7 @@
+import { readPost } from "../../api/post/read.js";
+
 alert("Single Post Page");
+
+const urlParams = new URLSearchParams(window.location.search);
+const postId = urlParams.get('id');
+await readPost(postId)
