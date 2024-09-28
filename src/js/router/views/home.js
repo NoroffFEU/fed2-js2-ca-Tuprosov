f113f226aee342d24287148b42776ad354a5a286
Shahzod <shahzodwill@gmail.com>
2024-09-28 16:43:48 +0200

    completed logout function

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -3,3 +3,10 @@ import { setLogoutListener } from "../../ui/global/logout.js";
 
 authGuard();
 setLogoutListener();
+
+const logOut = document.getElementById('logout');
+logOut.addEventListener('click', ()=> {
+    localStorage.clear();
+    alert('You have logged out!')
+    authGuard();
+})
