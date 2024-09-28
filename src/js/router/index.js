5af2dd46120e8a066d344fb9834c37f658d88b21
Shahzod <shahzodwill@gmail.com>
2024-09-28 12:03:42 +0200

    changed the path

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -5,30 +5,38 @@ import { readPosts } from "../api/post/read.js";
 
 export default async function router(pathname = window.location.pathname) {
   switch (pathname) {
+    case "/":
     case "/index.html":
       await import("./views/home.js");
       break;
+    case "/auth":
     case "/auth/index.html":
       await import("./views/auth.js");
       break;
+    case "/auth/login":
     case "/auth/login/index.html":
       await import("./views/login.js");
       break;
+    case "/auth/register":
     case "/auth/register/index.html":
       await import("./views/register.js");
       break;
+    case "/post":
     case "/post/index.html":
       await import("./views/post.js");
       break;
-    case "/post/listing.html":
+    case "/post/listing":
       await readPosts();
       break;
+    case "/post/edit":
     case "/post/edit/index.html":
       await import("./views/postEdit.js");
       break;
+    case "/post/create":
     case "/post/create/index.html":
       await import("./views/postCreate.js");
       break;
+    case "/profile":
     case "/profile/index.html":
       await import("./views/profile.js");
       break;
